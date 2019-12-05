bee3d038dd0a27c8734932e7ccb95de301c607dd
shion <tenta_shion@yahoo.co.jp>
2019-12-05 23:38:18 +0900

    C# 版の実装追加

---
+++ csharp/GraphModel.cs
@@ -0,0 +1,66 @@
+using System.Numerics;
+
+namespace MandelbrotSet
+{
+    /// <summary>
+    /// グラフ生成モデル
+    /// </summary>
+    class GraphModel
+    {
+        /// <summary>
+        /// マンデルブロ集合を生成する
+        /// </summary>
+        /// <param name="max">最大値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="size">一辺の大きさ</param>
+        /// <param name="limit">閾値</param>
+        /// <param name="maxCycle">最大試行回数</param>
+        public int[,] MakeMandelbrotSet(
+            Complex max,
+            Complex min,
+            int size,
+            int limit = 100,
+            int maxCycle = 500
+        )
+        {
+            var result = new int[size, size];
+
+            var scale = new Complex(
+                (max.Real - min.Real) / (size - 1),
+                (max.Imaginary - min.Imaginary) / (size - 1)
+            );
+
+            var table = new Complex[size];
+            table[0] = min;
+            for (int i = 1; i < size; i++)
+            {
+                table[i] = new Complex(
+                    table[i - 1].Real + scale.Real,
+                    table[i - 1].Imaginary + scale.Imaginary
+                );
+            }
+
+            var history = new Complex[maxCycle];
+            history[0] = new Complex(0, 0);
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    for (int cycle = 1; cycle < maxCycle; cycle++)
+                    {
+                        history[cycle] = Complex.Pow(history[cycle - 1], 2.0) + new Complex(table[row].Real, table[col].Imaginary);
+
+                        if (limit <= Complex.Abs(history[cycle]))
+                        {
+                            result[row, col] = (cycle % 255) + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
 
 namespace MandelbrotSet
 {
@@ -6,7 +9,37 @@ namespace MandelbrotSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size = 1024;
+            var model = new GraphModel();
+
+            Console.WriteLine("Start Calculation");
+            var swCalculation = Stopwatch.StartNew();
+            var map = model.MakeMandelbrotSet(
+                new Complex(0.50, 1.00),
+                new Complex(-1.50, -1.00),
+                size
+            );
+            swCalculation.Stop();
+            Console.WriteLine($"Finish Calculation: {swCalculation.ElapsedMilliseconds}[ms]");
+
+            Console.WriteLine("Start File Output");
+            var swOutput = Stopwatch.StartNew();
+            using (var writer = new StreamWriter("image.pgm"))
+            {
+                writer.WriteLine("P2");
+                writer.WriteLine($"{size} {size}");
+                writer.WriteLine("255");
+                for (int row = 0; row < size; row++)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        writer.Write($"{map[row, col]}\t");
+                    }
+                    writer.WriteLine();
+                }
+            }
+            swOutput.Stop();
+            Console.WriteLine($"Finish File Output: {swOutput.ElapsedMilliseconds}[ms]");
         }
     }
 }
