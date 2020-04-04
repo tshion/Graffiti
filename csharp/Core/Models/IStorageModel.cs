36ae73741952e7f5c01e7d7609e93fc6818fad63
shion <tenta_shion@yahoo.co.jp>
2020-04-04 23:35:23 +0900

    【C#】ビジネスロジックの整備

---
+++ csharp/Core/Models/IStorageModel.cs
@@ -0,0 +1,14 @@
+namespace Core.Models
+{
+    /// <summary>
+    /// ストレージロジックの定義
+    /// </summary>
+    public interface IStorageModel
+    {
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="dots">描画データ</param>
+        void Save(int[,] dots);
+    }
+}
