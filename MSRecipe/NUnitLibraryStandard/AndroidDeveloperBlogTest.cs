20252e2e32b7cbe60af5b9967f581d70973952c5
shion <tenta_shion@yahoo.co.jp>
2021-12-12 21:33:59 +0900

    Atom フィード解析の試し実装

---
+++ MSRecipe/NUnitLibraryStandard/AndroidDeveloperBlogTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace NUnitLibraryStandard
+{
+    [TestFixture]
+    internal class AndroidDeveloperBlogTest
+    {
+        [Test]
+        public void Test()
+        {
+            XNamespace xmlns = "http://www.w3.org/2005/Atom";
+            var root = XElement.Load("https://feeds.feedburner.com/blogspot/hsDu");
+
+            var entries = root.Descendants(xmlns + "entry");
+            var subtitle = root.Descendants(xmlns + "subtitle").FirstOrDefault();
+            var title = root.Descendants(xmlns + "title").FirstOrDefault();
+            var updated = root.Descendants(xmlns + "updated").FirstOrDefault();
+
+            var data = new
+            {
+                items = entries.Select(e => new
+                {
+                    content = e.Descendants(xmlns + "content").FirstOrDefault().Value,
+                    title = e.Descendants(xmlns + "title").FirstOrDefault().Value,
+                    updated = root.Descendants(xmlns + "updated").FirstOrDefault().Value,
+                }).ToList(),
+                subtitle = subtitle.Value,
+                title = title.Value,
+                updated = updated.Value,
+            };
+        }
+    }
+}
