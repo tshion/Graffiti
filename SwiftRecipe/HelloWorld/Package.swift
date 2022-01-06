02dafde0c0d6d301121606104a864ea801286a32
shion <tenta_shion@yahoo.co.jp>
2022-01-06 17:27:16 +0900

    配置場所の変更

--- SwiftRecipe/HelloWorld/Package.swift
+++ SwiftRecipe/HelloWorld/Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HelloWorld",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "HelloWorldTests",
-            dependencies: ["HelloWorld"]),
+            dependencies: ["HelloWorld"],
+            path: "Tests"),
     ]
 )
