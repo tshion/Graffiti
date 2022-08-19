a35e3e70811c2bd77bea8ba7ec0e47ff1b7fdb3e
shion <tenta_shion@yahoo.co.jp>
2022-08-20 08:58:31 +0900

    Android Jetpack のテスト追加

--- MSRecipe/NUnitLibraryStandard/FetchFeedTest.cs
+++ MSRecipe/NUnitLibraryStandard/FetchFeedTest.cs
@@ -36,5 +36,35 @@ namespace NUnitLibraryStandard
             Assert.IsNotEmpty(data.title);
             Assert.AreEqual("Android Developers Blog", data.title);
         }
+
+        [Test]
+        public void AndroidJetpack()
+        {
+            XNamespace xmlns = "http://www.w3.org/2005/Atom";
+            var root = XElement.Load("https://developer.android.com/feeds/androidx-release-notes.xml");
+
+            var entries = root.Descendants(xmlns + "entry");
+            var subtitle = root.Descendants(xmlns + "subtitle").FirstOrDefault();
+            var title = root.Descendants(xmlns + "title").FirstOrDefault();
+            var updated = root.Descendants(xmlns + "updated").FirstOrDefault();
+
+            var data = new
+            {
+                items = entries.Select(e => new
+                {
+                    content = e.Descendants(xmlns + "content").FirstOrDefault().Value,
+                    title = e.Descendants(xmlns + "title").FirstOrDefault().Value,
+                    updated = root.Descendants(xmlns + "updated").FirstOrDefault().Value,
+                }).ToList(),
+                subtitle = subtitle?.Value ?? "",
+                title = title.Value,
+                updated = updated.Value,
+            };
+
+
+            Assert.IsEmpty(data.subtitle);
+            Assert.IsNotEmpty(data.title);
+            Assert.AreEqual("AndroidX - Release Notes", data.title);
+        }
     }
 }
