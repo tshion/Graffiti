aa423b912c9c481630d3670ed86d42062bf67aa8
tshion <tenta_shion@yahoo.co.jp>
2023-10-09 13:58:32 +0900

    StrictMode のVM ポリシーの整備

--- android/app/src/debug/java/io/github/tshion/graffiti/DebugApplication.kt
+++ android/app/src/debug/java/io/github/tshion/graffiti/DebugApplication.kt
@@ -1,6 +1,42 @@
 package io.github.tshion.graffiti
 
+import android.os.Build
+import android.os.StrictMode
+
 /**
  * Debug ビルド時に適用するApplication クラス
  */
-class DebugApplication : MainApplication()
+class DebugApplication : MainApplication() {
+
+    override fun onCreate() {
+        // region: StrictMode の設定
+        StrictMode.VmPolicy.Builder()
+            // .detectActivityLeaks() // LeakCanary で代用
+            // .detectCleartextNetwork() // Android OS 9 以降はそもそも明示的な設定が必要なため、省略
+            .detectContentUriWithoutPermission()
+            .detectCredentialProtectedWhileLocked()
+            .detectFileUriExposure()
+            .detectImplicitDirectBoot()
+            .apply {
+                if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
+                    detectIncorrectContextUse()
+                }
+            }
+            .detectLeakedClosableObjects()
+            .detectLeakedRegistrationObjects()
+            .detectLeakedSqlLiteObjects()
+            .detectNonSdkApiUsage()
+            .apply {
+                if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
+                    detectUnsafeIntentLaunch()
+                }
+            }
+            .detectUntaggedSockets()
+            .penaltyDeath()
+            .build()
+            .also { StrictMode.setVmPolicy(it) }
+        // endregion
+
+        super.onCreate()
+    }
+}
